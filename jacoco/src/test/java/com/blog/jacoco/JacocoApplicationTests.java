03c134870afead5374cedb1238889a7cd7ddd104
csbsjy <seojaeyeon@lotte.net>
2019-12-20 14:45:58 +0900

    update jacoco

---
+++ jacoco/src/test/java/com/blog/jacoco/JacocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.jacoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ jacoco/src/test/java/com/blog/jacoco/domain/CalculatorTest.java
@@ -0,0 +1,46 @@
+package com.blog.jacoco.domain;
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void 정수와_정수를_더한다() {
+        long result = calculator.add(2147483647, 1);
+        assertThat(result).isEqualTo(2147483648L);
+    }
+
+    @Test
+    void 정수와_정수를_뺀다() {
+        long result = calculator.subtract(5, 100);
+        assertThat(result).isEqualTo(-95L);
+    }
+
+    @Test
+    void 정수와_정수를_곱한다() {
+        long result = calculator.multiply(5, 100);
+        assertThat(result).isEqualTo(500);
+
+    }
+
+    @Test
+    void 정수와_정수를_나눈다() {
+        long result = calculator.divide(100, 5);
+        assertThat(result).isEqualTo(20);
+
+    }
+
+
+    @Test
+    void 영으로_나누면_에러(){
+        assertThatThrownBy(() -> calculator.divide(100, 0)).isInstanceOf(IllegalArgumentException.class);
+
+    }
+}
